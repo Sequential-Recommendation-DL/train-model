1017ec44621079acda87aca1ed37b6ea608ceb83
VD-Phuc-2k5 <duyvo1954@gmail.com>
2026-05-27 13:47:57 +0700

    feat: environment configuartion

--- flake.nix
+++ flake.nix
@@ -12,24 +12,29 @@
       pkgs = import nixpkgs { inherit system; };
     in
     {
-       devShells.${system}.default = pkgs.mkShell {
-         buildInputs = with pkgs; [
-           (pkgs.python312.withPackages (
-             ps: with ps; [
-               numpy
-               pandas
-               torch
-               torchvision
-               matplotlib
-               ruff
-               black
-               mypy
-               pip
-             ]
-           ))
-         ];
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          (pkgs.python312.withPackages (
+            ps: with ps; [
+              numpy
+              pandas
+              torch
+              torchvision
+              matplotlib
+              ruff
+              black
+              mypy
+              pip
+              huggingface-hub
+              orjson
+            ]
+          ))
+        ];
 
         shellHook = ''
+          if [ -f .env ]; then
+            export $(grep -v '^#' .env | xargs)
+          fi
           echo "Development environment configuration ready!" 
           echo "Python: $(python --version)"
         '';
